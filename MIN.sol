309b4d4291862b92351d32fd5a6087381a8889e2
Mutual insurance network <82998340+minnetwork@users.noreply.github.com>
2021-04-25 11:41:13 +0800

    Delete MIN.sol

--- MIN.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity 0.5.16;
-
-import "./BEP20Token.sol";
-
-contract MIN is BEP20Token{
-    constructor() BEP20Token("Mutual Insurance Network LP","MIN-LP",18,1e27) public{       
-    }
-
-}
